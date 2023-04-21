dbd76975511cbc971a6c0f04705ba54a7baa232a
jayoonmaeng <jayun951@gmail.com>
2023-04-21 15:19:25 +0900

    0421

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-// .button 을 클릭 했을때, **
-// input안에 있는 값을 가져와야함.
+const divider = document.querySelector(".divider");
+const leftPane = document.querySelector(".left-pane");
+const rightPane = document.querySelector(".right-pane");
+
+let isDragging = false;
+
+divider.addEventListener("mousedown", (e) => {
+  isDragging = true;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+
+  const containerWidth = document.querySelector(".container").offsetWidth;
+  const dividerWidth = divider.offsetWidth;
+  const x = e.pageX - dividerWidth / 2;
+  const leftPaneWidth = x;
+  const rightPaneWidth = containerWidth - x - dividerWidth;
+
+  leftPane.style.width = leftPaneWidth + "px";
+  rightPane.style.width = rightPaneWidth + "px";
+});
+
+document.addEventListener("mouseup", (e) => {
+  isDragging = false;
+});
